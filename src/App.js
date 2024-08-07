1dd009b91653d9b4a9abc3afb37feaa984189bcf
Lucas Hahne <lucas-hahne@web.de>
2024-08-07 22:38:16 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Player from "./components/Player";
+import Song from "./components/Song";
+import { chillHop } from "./util";
+import "./styles/App.scss";
+import { useState } from "react";
 
 function App() {
+  const [songs, setSongs] = useState(chillHop());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Song currentSong={currentSong}></Song>
+      <Player></Player>
     </div>
   );
 }
